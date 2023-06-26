5b93ce3650866e88811a2d29a697dae04cf63146
devZahid <officialzahidhasan19932021@gmail.com>
2023-06-26 17:42:40 +0600

    connect mongoDB via mongoose

--- index.js
+++ index.js
@@ -2,27 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-
-
 //mongo connections
 
 async function main() {
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to mongoDB');
-    } catch (error) {
-        throw error;
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    // console.log('Connected to mongoDB');
+  } catch (error) {
+    throw error;
   }
+}
 
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
 
-app.listen(port, ()=> {
-    main()
-    console.log("app running on port:",port);
-})
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB Connected!!!");
+});
+
+app.listen(port, () => {
+  main();
+  console.log("app running on port:", port);
+});
